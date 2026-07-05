c39775d6bba43d1ef72a9e59e6c1c83d3a8e0a48
Bhuwan Khadka <bhuwankhadka2003@gmail.com>
2026-07-06 05:28:38 +0545

    Create Category model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from models import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.Enum("Admin", "User"), default="User")
+    created_at = db.Column(db.DateTime)
