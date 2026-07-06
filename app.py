ce8f4a1c3bcaa824bdced3bdd7a0b62dc182a971
Bhuwan Khadka <bhuwankhadka2003@gmail.com>
2026-07-06 06:53:33 +0545

    Configure Flask-Login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import Config
 from models import db
 
@@ -6,6 +7,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
 @app.route("/")
 def home():
